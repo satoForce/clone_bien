0274f69f7af3750a5821b3a2b0bc2204ec036275
fasebas <fasesize@hotmail.com>
2023-10-12 11:17:49 -0500

    arreglo journey - wip

--- Well/Tabs/JourneyTabs/JourneysTab.swift
+++ Well/Tabs/JourneyTabs/JourneysTab.swift
@@ -12,33 +12,28 @@ import SwiftUI
 
 struct JourneysTab: View {
     @State private var selectedTab = 0
-    private let tabCount = 3 // Cantidad de pestañas
-
+    private let tabCount = 3
     var body: some View {
-        
         NavigationView {
             VStack(alignment: .leading,spacing: 0) {
-                // ScrollView(.horizontal, showsIndicators: false) {
                 HStack(spacing: 20) {
                     ForEach(0..<tabCount, id: \.self) { index in
                         Button(action: {
                             self.selectedTab = index
                         }) {
                             Text("\(tabs[index])")
-                                .font(.headline)
+                                .font(.system(size: 20))
                                 .padding(.vertical, 10)
                                 .foregroundColor(selectedTab == index ? .blue : .black)
                         }
                     }
                 }
-                //}
                 Divider().background(Color.black)
                 GeometryReader { geometry in
                     Rectangle()
                         .frame(width: (geometry.size.width / 2) / CGFloat(tabCount) , height: 2)
                         .foregroundColor(.blue)
                         .offset(x: CGFloat(selectedTab) * (geometry.size.width / 2) / CGFloat(tabCount))
-                    
                     if selectedTab == 0 {
                         NewForYou()
                     } else if selectedTab == 1 {
@@ -46,16 +41,11 @@ struct JourneysTab: View {
                     } else {
                         Past()
                     }
-                    
                 }
-                
-                
-                
                 Spacer()
             }
             .padding()
-        }//.navigationTitle("Journeys")
-        
+        }
     }
 }
 
--- Well/Tabs/JourneyTabs/NewForYou/Carousel5.swift
+++ Well/Tabs/JourneyTabs/NewForYou/Carousel5.swift
@@ -9,65 +9,49 @@ import SwiftUI
 
 struct Carousel5: View {
     var body: some View {
-    
-                // VIEW 2
-        ScrollView(.horizontal, showsIndicators: true) {
-            HStack(spacing: 10) {
+        ScrollView(.horizontal, showsIndicators: false) {
+            HStack(spacing: 20) {
                 ForEach(0..<5) { index in
-                    NavigationLink(destination: JourneyDetail()) {
-                        ZStack(alignment: .leading) {
-                            Rectangle()
-                                .fill(LinearGradient(gradient: Gradient(colors: [.purple, .blue]), startPoint: .topLeading, endPoint: .bottomTrailing))
-                                .frame(width: 340, height: 300) // Ajusta el tamaño aquí
-                                .cornerRadius(10)
-                                .shadow(color: Color.black.opacity(0.3), radius: 5, x: 0, y: 5)
-                                .padding()
-
-                            
-                            VStack(alignment: .leading, spacing: 40) {
-                                Rectangle()
-                                    .foregroundColor(Color.black.opacity(0.3))
-                                    .frame(width: 110, height: 30) // Tamaño del cuadro amarillo
-                                    .cornerRadius(10)
-                                    .overlay(
-                                        HStack {
-                                            Image(systemName: "circle.fill")
-                                                .foregroundColor(Color.yellow)
-                                                .font(Font.system(size: 15)) // Ajusta el tamaño de la fuente
-                                            
-                                            Text("10 per day")
-                                                .foregroundColor(.white) // Texto blanco
-                                                .font(Font.system(size: 15)) // Ajusta el tamaño de la fuente
-                                        }
-                                    )
-                                
-                                VStack (alignment: .leading){
-                                    Text("Title")
-                                        .foregroundColor(Color.white)
-                                    .font(.headline)
-                                    
-                                    Text("Texto largo que va como complemento del subtitulo.")
-                                                                    .foregroundColor(Color.white)
-                                                                    .font(.subheadline)
-                                                                    .multilineTextAlignment(.leading)
-                                }
-                                .padding(.vertical,30)
-                                
-                                
+                    ZStack {
+                        VStack (alignment: .leading){
+                            ZStack {               
+                                HStack {
+                                        Image(systemName: "circle.fill")
+                                            .foregroundColor(Color.yellow)
+                                            .font(.body)
+                                        Text("10 per day")
+                                            .foregroundColor(.white)
+                                            .font(.body)
+                                    }
+                                    .padding(7)
+                                    .font(.body)
                             }
-                            .frame(width: 240) // Ajusta el tamaño aquí
-                            .padding()
-                            .offset(y: 30)
+                            .background(Color.black.opacity(0.3))
+                            .cornerRadius(10)
+                            .padding(20)
+                            VStack(alignment: .leading, spacing: 2) {
+                                Text("Title")
+                                    .foregroundColor(Color.black)
+                                    .font(.title2)
+                                
+                                Text("Texto largo que va como complemento del subtitulo.")
+                                    .foregroundColor(Color.black)
+                                    .font(.body)
+                            }
+                            .lineLimit(nil)
+                            .fixedSize(horizontal: false, vertical: true)
+                            .padding(20)
                         }
-                    }//.navigationTitle("Learn more")
+                        .frame(maxWidth: .infinity, alignment: .leading)
+                        .background(Color.white)
+                        .cornerRadius(20)
+                        .shadow(color: Color.black.opacity(0.2), radius: 5, x: 0, y: 5)
+                        
+                    }.frame(width: UIScreen.main.bounds.width / 2)
                 }
             }
-            
+            .padding()
         }
-
-            
-        
-
     }
 }
 
