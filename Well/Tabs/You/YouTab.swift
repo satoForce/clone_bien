4a948caafc0f730f57de4f344fe997559dfd70f6
fasebas <fasesize@hotmail.com>
2023-10-13 09:54:47 -0500

    nuevoArchivoYou

---
+++ Well/Tabs/You/YouTab.swift
@@ -0,0 +1,18 @@
+//
+//  SwiftUIView.swift
+//  Well
+//
+//  Created by Fabrizio on 13/10/23.
+//
+
+import SwiftUI
+
+struct YouTab: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    YouTab()
+}
